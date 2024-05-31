a53228b8a1576664ce9752c5c2b905489fb83c45
Iskrisss <klfpaz@scl.edu.gt>
2024-05-30 18:47:37 -0600

    Section 2 Kristhel

--- section/section2.js
+++ section/section2.js
@@ -1,4 +1,51 @@
-let sect2 = document.querySelector(".section1");
-sect2.innerHTML=`
-<section class="section1"></section>
+let sect2 = document.querySelector(".section2");
+sect2.innerHTML=`<div class="table-container">
+<h1>Principales Etiquetas HTML</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Etiqueta</th>
+          <th>Descripción</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>header</td>
+          <td>Representa un contenedor para contenido introductorio o navegacional</td>
+        </tr>
+        <tr>
+          <td>nav</td>
+          <td>Define un conjunto de enlaces de navegación</td>
+        </tr>
+        <tr>
+          <td>footer</td>
+          <td>Define un pie de página para un documento o sección</td>
+        </tr>
+        <tr>
+          <td>section</td>
+          <td>Define una sección en un documento</td>
+        </tr>
+        <tr>
+          <td>article</td>
+          <td>Define contenido independiente y auto-contenido</td>
+        </tr>
+        <tr>
+          <td>div</td>
+          <td>Define una sección o división en un documento</td>
+        </tr>
+        <tr>
+          <td>span</td>
+          <td>Define una sección en línea en un documento</td>
+        </tr>
+        <tr>
+          <td>button</td>
+          <td>Define un botón clicable</td>
+        </tr>
+        <tr>
+          <td>table</td>
+          <td>Define una tabla</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 `;
